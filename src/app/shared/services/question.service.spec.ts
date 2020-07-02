8ab908f5fe76682e6a33036182cd377ddf6f9e89
Raghavendra Koppula <54515432+rpranith@users.noreply.github.com>
2020-07-02 14:38:43 -0400

    added getQuestion test

--- src/app/shared/services/question.service.spec.ts
+++ src/app/shared/services/question.service.spec.ts
@@ -1,10 +1,11 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed, inject, tick } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { QuestionService } from './question.service';
 
 describe('QuestionService', () => {
   let service: QuestionService;
+  let todayquestionapi = "http://localhost:3000/api/questions/today-question";
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,5 +16,32 @@ describe('QuestionService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getQuestions()', () => {
+    it('should return a get call and have defined properties of a question', ()=> {
+      inject(
+        [HttpTestingController],
+        (backend: HttpTestingController) => {
+
+          let response = null;
+
+          service.getTodayQuestion().subscribe((res: any) => {
+            response = res;
+          });
+
+          const requestWrapper = backend.expectOne({ url: todayquestionapi });
+
+          tick();
+
+          expect(response.status).toBe(200);
+          expect(requestWrapper.request.method).toEqual('GET');
+          expect(response.question).toBeDefined();
+          expect(response.answers).toBeDefined();
+          expect(response.reasoning).toBeDefined();
+          expect(response.correct_answer).toBeDefined();
+        }
+      )
+    });
   });
 });
